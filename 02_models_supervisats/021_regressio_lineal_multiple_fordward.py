b738c3a9b16d773560961d06bd0860c5557151e5
robertventura <rventura@sapalomera.cat>
2022-12-16 11:57:27 +0100

    Afegeixo l'exemple de regressio lineal multiple Fordward Selecton.
    La implamentació amb python encara cal corregir-la

---
+++ 02_models_supervisats/021_regressio_lineal_multiple_fordward.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+# Importem el dataset
+df = pd.read_csv('dataset/50_Startups.csv')
+df.head()
+
+# Dividim el dataframe amb les variables independents (X) i les dependents (Y)
+vars_x = ['R&D Spend', 'Administration', 'Marketing Spend', 'State']
+var_y = 'Profit'
+
+x = df[vars_x]
+y = df[var_y]
+
+# Construim les variables dummy a partir de la variable categòrica State
+x = pd.get_dummies(x,columns=["State"],drop_first=True)
+x.head()
+vars_x.remove('State') # Traiem la vairable categòrica State
+
+vars_x = x.columns
+vars_x = vars_x.to_list()
+print(vars_x)
+#Afegim una variable constant.
+x = sm.add_constant(x)
+
+# Dividim el dataset amb dades de test i de train. 30% test 70% train
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3, random_state = 0)
+
+## x_opt és el conjunt de variables independents òptimes / significatives
+## per predir la y.
+vars_x_opt = [] #vars_x.copy()
+
+# Preparem una llista de candidats
+#candidates = []
+last_pvalor_max = -1
+
+SL = 0.05
+fi = False
+print(vars_x)
+while(len(vars_x)==0 or not fi):
+    x_pvalors = dict()
+    
+    # Calculem tots els models amb les variables x que ens quedin.
+    #Per construir un diccionari a on hi hagi per cada VI el pvalor.
+    for vi_indep in vars_x :
+        vars_x_tmp = vars_x_opt.copy() # Variable  que ens servirà per guardar         
+        vars_x_tmp.append(vi_indep)
+        # Generem el model     
+        print(vars_x_tmp)
+        model = sm.OLS(endog = y_train, exog = x_train[vars_x_tmp])
+        results = model.fit()
+        pvalors = results.pvalues.to_dict()
+        
+        x_pvalors[vi_indep] = pvalors[vi_indep]
+    
+    # Un cop calculats tots els models ens quedarem amb el model 
+    # que té el valor mínim.    
+    min_pvalor =  min(x_pvalors.values())
+    min_pvalor_key = min(x_pvalors, key=x_pvalors.get)
+    
+    if min_pvalor < SL:
+        #Afegim la VI a la llista de var_x_opt
+        vars_x_opt.append(min_pvalor_key)        
+        #Traiem de la llista de candidats
+        print("Traiem el valor {}",min_pvalor_key)
+        print(vars_x[0])    
+        vars_x.remove('R&D Spend')
+    else:
+        #Finalitzem perquè ja no hi ha cap model amb pvalor < SI
+        fi = True
+
+#Un cop hem finalitzat tenim les variables VI òptimes. Per tant calculem el model amb aquestes
+model = sm.OLS(endog = y_train, exog = x_train[vars_x_opt])
+results = model.fit()
+pvalors = results.pvalues.to_dict()
+
+print(results.summary())
