ccc4064cb1230f274959930053b6301bff1798c0
robertventura <rventura@sapalomera.cat>
2022-12-26 19:29:32 +0100

    Arreglo alguns error i trec variables inecessàries

--- 02_models_supervisats/021_regressio_lineal_multiple_fordward.py
+++ 02_models_supervisats/021_regressio_lineal_multiple_fordward.py
@@ -21,22 +21,19 @@ x = pd.get_dummies(x,columns=["State"],drop_first=True)
 x.head()
 vars_x.remove('State') # Traiem la vairable categòrica State
 
-vars_x = x.columns
-vars_x = vars_x.to_list()
-print(vars_x)
 #Afegim una variable constant.
 x = sm.add_constant(x)
+#print(x)
+
+vars_x = x.columns
+vars_x = vars_x.to_list()
 
 # Dividim el dataset amb dades de test i de train. 30% test 70% train
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3, random_state = 0)
 
-## x_opt és el conjunt de variables independents òptimes / significatives
+## vars_x_opt és el conjunt de variables independents òptimes / significatives
 ## per predir la y.
-vars_x_opt = [] #vars_x.copy()
-
-# Preparem una llista de candidats
-#candidates = []
-last_pvalor_max = -1
+vars_x_opt = []
 
 SL = 0.05
 fi = False
@@ -45,20 +42,22 @@ while(len(vars_x)==0 or not fi):
     x_pvalors = dict()
     
     # Calculem tots els models amb les variables x que ens quedin.
-    #Per construir un diccionari a on hi hagi per cada VI el pvalor.
+    # Per construir un diccionari a on hi hagi per cada VI el pvalor.
     for vi_indep in vars_x :
-        vars_x_tmp = vars_x_opt.copy() # Variable  que ens servirà per guardar         
+        vars_x_tmp = vars_x_opt.copy() # Variable temporal per no utilitzar vars_x_opt
         vars_x_tmp.append(vi_indep)
         # Generem el model     
+        print("\nGenerem el model per:")
         print(vars_x_tmp)
         model = sm.OLS(endog = y_train, exog = x_train[vars_x_tmp])
         results = model.fit()
         pvalors = results.pvalues.to_dict()
         
         x_pvalors[vi_indep] = pvalors[vi_indep]
+        print("La variable '{}' té un p-valor {}".format(vi_indep,pvalors[vi_indep]))        
     
     # Un cop calculats tots els models ens quedarem amb el model 
-    # que té el valor mínim.    
+    # que té el p-valor mínim.    
     min_pvalor =  min(x_pvalors.values())
     min_pvalor_key = min(x_pvalors, key=x_pvalors.get)
     
@@ -66,11 +65,13 @@ while(len(vars_x)==0 or not fi):
         #Afegim la VI a la llista de var_x_opt
         vars_x_opt.append(min_pvalor_key)        
         #Traiem de la llista de candidats
-        print("Traiem el valor {}",min_pvalor_key)
-        print(vars_x[0])    
-        vars_x.remove('R&D Spend')
+        print("-------")
+        print("------> Escollim la variable '{}' amb un p-valor {}".format(min_pvalor_key,min_pvalor))
+        print("-------")
+        #print(vars_x[0])    
+        vars_x.remove(min_pvalor_key)
     else:
-        #Finalitzem perquè ja no hi ha cap model amb pvalor < SI
+        #Finalitzem perquè ja no hi ha cap model amb pvalor < SL
         fi = True
 
 #Un cop hem finalitzat tenim les variables VI òptimes. Per tant calculem el model amb aquestes
